f845048ef4150c9e8754d887e25cef5467690c4f
Phumrapee Limpianchop <git@rayriffy.com>
2019-01-21 14:52:12 +0700

    :fire: Remove unused modules

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,11 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import { graphql } from 'gatsby'
 import Grid from '@material-ui/core/Grid'
-import Paper from '@material-ui/core/Paper'
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-
-import LazyLoad from 'react-lazyload'
 
 class MainPage extends React.Component {
   render() {
