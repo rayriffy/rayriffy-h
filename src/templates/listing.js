0ba40100ad8a16ad3c56ec318cfbb4de15ad2be0
Phumrapee Limpianchop <git@rayriffy.com>
2019-04-09 17:12:50 +0700

    :sparkles: Interactive frontend

--- src/templates/listing.js
+++ src/templates/listing.js
@@ -3,10 +3,14 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import {graphql} from 'gatsby'
 
-import Style from '../components/style.module.css'
-import Grid from '@material-ui/core/Grid'
+import {Row, Col, Layout, Typography, Card, Tag} from 'antd'
+import 'antd/dist/antd.css'
 
 import LazyLoad from 'react-lazyload'
+
+const {Footer, Content} = Layout
+const {Title} = Typography
+const {Meta} = Card
 
 class MainPage extends React.Component {
   render() {
@@ -15,30 +19,60 @@ class MainPage extends React.Component {
     const {raw} = this.props.pageContext
 
     return (
-      <div className={Style.container}>
+      <div>
         <Helmet htmlAttributes={{lang: 'en'}} title={`${siteTitle}`} />
-
-        <Grid container spacing={24} alignItems="center">
-          {raw.map(node => {
-            if (node.status === 'success') {
-              return (
-                <Grid item xs={12} sm={6} md={4} key={`grid-${node.data.id}`}>
-                  <a href={`/c/${node.data.id}`} target="_blank" rel="noopener noreferrer">
-                    <LazyLoad>
-                      <img
-                        src={`https://t.nhentai.net/galleries/${node.data.raw.media_id}/cover.${
-                          node.data.raw.images.cover.t === 'p' ? 'png' : 'jpg'
-                        }`}
-                      />
-                    </LazyLoad>
-                    <br />
-                    {node.data.raw.title.pretty}
-                  </a>
-                </Grid>
-              )
-            }
-          })}
-        </Grid>
+        <Layout>
+          <Content style={{padding: '0 25px'}}>
+            <Row>
+              <Title style={{fontSize: '42px', marginTop: '20px', marginLeft: '15px', fontWeight: 700}}>Riffy H</Title>
+            </Row>
+            <Row gutter={16} type="flex" justify="space-around" align="middle">
+              {raw.map(node => {
+                if (node.status === 'success') {
+                  return (
+                    <Col
+                      style={{padding: '5px 5px'}}
+                      xs={{span: 24}}
+                      sm={{span: 12}}
+                      md={{span: 8}}
+                      lg={{span: 6}}
+                      xl={{span: 4}}
+                      key={`grid-${node.data.id}`}>
+                      <a href={`/c/${node.data.id}`} target="_blank" rel="noopener noreferrer">
+                        <Card
+                          hoverable
+                          cover={
+                            <LazyLoad>
+                              <img
+                                alt="cover"
+                                src={`https://t.nhentai.net/galleries/${node.data.raw.media_id}/cover.${
+                                  node.data.raw.images.cover.t === 'p' ? 'png' : 'jpg'
+                                }`}
+                              />
+                            </LazyLoad>
+                          }>
+                          <Meta
+                            title={node.data.raw.title.pretty}
+                            description={node.data.raw.tags.map(tag => {
+                              if (tag.type === 'tag') {
+                                return <Tag color="#108ee9">{tag.name}</Tag>
+                              } else if (tag.type === 'parody') {
+                                return <Tag color="#f50">{tag.name}</Tag>
+                              }
+                            })}
+                          />
+                        </Card>
+                      </a>
+                    </Col>
+                  )
+                }
+              })}
+            </Row>
+          </Content>
+          <Footer>
+            Fork this project on <a href="https://github.com/rayriffy/rayriffy-h">GitHub</a>
+          </Footer>
+        </Layout>
       </div>
     )
   }
