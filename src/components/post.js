88c458a56189e18f2518fbdc5cf1ab043efa6f86
Phumrapee Limpianchop <git@rayriffy.com>
2019-04-10 15:49:41 +0700

    :sparkles: Share button and custom ID

---
+++ src/components/post.js
@@ -0,0 +1,174 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import axios from 'axios'
+import {CopyToClipboard} from 'react-copy-to-clipboard'
+
+import {Row, Col, Typography, Tag, Button, Icon, Modal, Skeleton, Divider} from 'antd'
+
+const {Title, Text} = Typography
+
+export class Post extends React.Component {
+  state = {visible: false, isLoading: true, hits: null, error: false, isCopied: false}
+
+  showModal = async id => {
+    this.setState({
+      visible: true,
+      isLoading: true,
+    })
+
+    try {
+      const out = await axios.get(`https://opener.now.sh/api/generate/${id}`)
+      this.setState({hits: out.data.dataURL, isLoading: false})
+    } catch (err) {
+      console.log(err)
+      this.setState({error: true, isLoading: false})
+    }
+  }
+
+  handleOk = e => {
+    this.setState({
+      visible: false,
+    })
+  }
+
+  handleCancel = e => {
+    this.setState({
+      visible: false,
+    })
+  }
+
+  render() {
+    const {visible, isLoading, error, hits, isCopied} = this.state
+    const {raw, post = {exclude: []}} = this.props
+
+    const pages = raw.images.pages
+
+    return (
+      <div>
+        <Row style={{maxHeight: '460px'}} key="row-meta">
+          <Col
+            xs={{span: 10, offset: 2}}
+            sm={{span: 8, offset: 4}}
+            md={{span: 6, offset: 6}}
+            lg={{span: 4, offset: 8}}
+            key="col-cover">
+            <img
+              alt="cover"
+              style={{width: '100%', height: 'auto'}}
+              src={`https://t.nhentai.net/galleries/${raw.media_id}/cover.${raw.images.cover.t === 'p' ? 'png' : 'jpg'}`}
+            />
+          </Col>
+          <Col
+            xs={{span: 9, offset: 1}}
+            sm={{span: 7, offset: 1}}
+            md={{span: 5, offset: 1}}
+            lg={{span: 3, offset: 1}}
+            key="col-meta">
+            <Row key="meta-title">
+              <Title level={3}>{raw.title.pretty}</Title>
+            </Row>
+            <Row key="meta-tag">
+              <Text strong style={{marginRight: 8, display: 'inline'}}>
+                Tag
+              </Text>
+              {raw.tags.map(tag => {
+                if (tag.type === 'tag') {
+                  return (
+                    <Tag color="blue" key={`tag-${raw.id}-${tag.id}`}>
+                      {tag.name}
+                    </Tag>
+                  )
+                }
+              })}
+            </Row>
+            <Row key="meta-buttons">
+              <Button
+                type="primary"
+                shape="circle"
+                style={{marginTop: '20px', marginRight: '10px'}}
+                key="button-share"
+                onClick={() => this.showModal(raw.id)}>
+                <Icon type="share-alt" />
+              </Button>
+              <Button shape="circle" style={{marginTop: '20px'}} key="button-export">
+                <a href={`https://nhentai.net/g/${raw.id}`} target="_blank" rel="noopener noreferrer">
+                  <Icon type="export" />
+                </a>
+              </Button>
+              <Modal
+                title="Share"
+                visible={visible}
+                onCancel={this.handleCancel}
+                footer={[
+                  <Button key="back" onClick={this.handleCancel}>
+                    Close
+                  </Button>,
+                ]}>
+                {isLoading ? (
+                  <Skeleton active />
+                ) : error ? (
+                  <div>Connot retrive data from API :(</div>
+                ) : (
+                  <div>
+                    <Row>
+                      <Col xs={{span: 12, offset: 6}}>
+                        <img style={{borderRadius: '10px', width: '100%'}} src={hits} />
+                      </Col>
+                      <Col style={{marginTop: '10px', textAlign: 'center'}} xs={{span: 16, offset: 4}}>
+                        <Text strong>Share securely with Opener</Text>
+                      </Col>
+                    </Row>
+                    <Row style={{marginTop: '25px'}}>
+                      <Col xs={{span: 11, offset: 1}}>
+                        <a href={hits} download>
+                          <Button type="primary" style={{width: '100%'}}>
+                            <Icon type="download" /> Download
+                          </Button>
+                        </a>
+                      </Col>
+                      <Col xs={{span: 10, offset: 1}}>
+                        <CopyToClipboard text={raw.id} onCopy={() => this.setState({isCopied: true})}>
+                          <Button style={{width: '100%'}}>
+                            <Icon type="copy" /> {isCopied ? `Copied!` : `Copy code`}
+                          </Button>
+                        </CopyToClipboard>
+                      </Col>
+                    </Row>
+                  </div>
+                )}
+              </Modal>
+            </Row>
+          </Col>
+        </Row>
+        <Divider>
+          <Icon type="book" theme="outlined" />
+        </Divider>
+        <Row key="row-images">
+          {pages.map((page, i) => {
+            if (!post.exclude.includes(i + 1)) {
+              return (
+                <Col
+                  xs={{span: 24, offset: 0}}
+                  sm={{span: 22, offset: 1}}
+                  md={{span: 20, offset: 2}}
+                  lg={{span: 16, offset: 4}}
+                  xl={{span: 12, offset: 6}}
+                  key={`image-${raw.id}-${i + 1}`}>
+                  <img
+                    style={{width: '100%'}}
+                    src={`https://i.nhentai.net/galleries/${raw.media_id}/${i + 1}.${page.t === 'p' ? 'png' : 'jpg'}`}
+                  />
+                </Col>
+              )
+            }
+          })}
+        </Row>
+      </div>
+    )
+  }
+}
+
+Post.propTypes = {
+  raw: PropTypes.object,
+  post: PropTypes.object,
+}
---
+++ src/pages/custom.js
@@ -0,0 +1,131 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import PropTypes from 'prop-types'
+import {graphql} from 'gatsby'
+import axios from 'axios'
+
+import {Row, Col, Card, Typography, Icon, Input, Skeleton} from 'antd'
+
+import {App} from '../components/app'
+import {Post} from '../components/post'
+
+const {Title, Text} = Typography
+
+class CustomPage extends React.Component {
+  state = {state: 0, subtitle: 'custom', inputValue: 0, raw: {}}
+
+  // constructor (props) {
+  //   super(props)
+  // }
+
+  find = async id => {
+    this.setState({
+      state: 1,
+      subtitle: 'processing',
+    })
+
+    try {
+      const out = await axios.get(`https://nh-express-git-master.rayriffy.now.sh/api/gallery/${id}`)
+      this.setState({raw: out.data, state: 4, subtitle: 'viewing'})
+    } catch (err) {
+      console.log(err)
+      this.setState({state: 3, subtitle: 'error'})
+    }
+  }
+
+  updateInputValue = val => {
+    this.setState({
+      inputValue: val.target.value,
+    })
+  }
+
+  componentDidMount() {
+    const getParams = location => {
+      const searchParams = new URLSearchParams(location.search)
+      return searchParams.get('query') || null
+    }
+
+    const {location} = this.props
+
+    if (getParams(location) !== null) {
+      this.find(getParams(location))
+    }
+  }
+
+  render() {
+    const siteTitle = this.props.data.site.siteMetadata.title
+
+    const {state, subtitle, inputValue, raw} = this.state
+
+    return (
+      <App title={siteTitle} subtitle={subtitle}>
+        <Helmet htmlAttributes={{lang: 'en'}} title={`${subtitle} · ${siteTitle}`} />
+        {state === 4 ? (
+          <Post raw={raw} />
+        ) : (
+          <Row>
+            <Col xs={{span: 20, offset: 2}} sm={{span: 16, offset: 4}} md={{span: 12, offset: 6}} lg={{span: 8, offset: 8}}>
+              {state === 0 || state === 3 ? (
+                <Card
+                  actions={[
+                    <a href="/" key="button-close">
+                      <Icon type="close-circle" />
+                    </a>,
+                    <a key="button-go" onClick={() => this.find(inputValue)}>
+                      <Icon type="check-circle" />
+                    </a>,
+                  ]}>
+                  <Row>
+                    <Title level={2}>Custom</Title>
+                    {state === 0 ? (
+                      <p>
+                        Input your code here <Text code>https://nhentai.net/g/:id</Text>
+                      </p>
+                    ) : (
+                      <p>Your request ID is not found</p>
+                    )}
+                  </Row>
+                  <Row>
+                    <Input placeholder="000000" size="large" onChange={this.updateInputValue} />
+                  </Row>
+                </Card>
+              ) : state === 1 ? (
+                <Card>
+                  <Row>
+                    <Title level={2}>Custom</Title>
+                    <Skeleton active />
+                  </Row>
+                </Card>
+              ) : (
+                <div>any</div>
+              )}
+            </Col>
+          </Row>
+        )}
+      </App>
+    )
+  }
+}
+
+export default CustomPage
+
+export const indexQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
+
+CustomPage.propTypes = {
+  location: PropTypes.object,
+  data: PropTypes.shape({
+    site: PropTypes.shape({
+      siteMetadata: PropTypes.shape({
+        title: PropTypes.string,
+      }),
+    }),
+  }),
+}
