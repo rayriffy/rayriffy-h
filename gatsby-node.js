fca1b951040d6ee04292c21d3d917e9652a227cb
Phumrapee Limpianchop <git@rayriffy.com>
2019-06-19 22:14:55 +0700

    :building_construction: Architectural changes

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,279 +8,271 @@ const {TaskQueue} = require('cwait')
 const MAX_SIMULTANEOUS_DOWNLOADS = 3
 const PREFETCH_GIST = 'https://gist.githubusercontent.com/rayriffy/09554279046d2fda29c125e0a16dc695/raw/crawler.json'
 
-exports.createPages = ({graphql, actions, reporter}) => {
+exports.createPages = async ({graphql, actions, reporter}) => {
   const {createPage} = actions
 
-  return new Promise((resolve, reject) => {
-    resolve(
-      graphql(
-        `
-          {
-            allDataJson {
-              edges {
-                node {
-                  nh_id
-                }
-              }
-            }
-            allTagJson {
-              edges {
-                node {
-                  prefix
-                  name
-                }
-              }
+  const result = await graphql(
+    `
+      {
+        allDataJson {
+          edges {
+            node {
+              nh_id
             }
           }
-        `,
-      )
-        .then(async result => {
-          if (result.errors) {
-            console.log(result.errors)
-            reject(result.errors)
-          }
-
-          /**
-           * Functions for query data from NHentai API
-           * @param {int} id Gallery ID
-           */
-          const getRawData = async id => {
-            try {
-              let isCacheFound = false
-              let cacheRes
-              // Read file from cache
-              if (fs.existsSync('.tmp/crawler.json')) {
-                const reader = fs.readFileSync('.tmp/crawler.json', 'utf8')
-                const objects = JSON.parse(reader)
-
-                _.each(objects, object => {
-                  if (object.data.id === id && object.status === 'success') {
-                    isCacheFound = true
-                    cacheRes = object
-                  }
-                })
-              }
-
-              if (isCacheFound) {
-                return cacheRes
-              } else {
-                // Using reverse proxy server to avoid CORS issue
-                const out = await axios.get(`https://nh-express-git-master.rayriffy.now.sh/api/gallery/${id}`)
-                return {
-                  status: 'success',
-                  data: {
-                    id: id,
-                    raw: out.data,
-                  },
-                }
-              }
-            } catch (err) {
-              reporter.warn(`cannot process ${id} with code ${err.code}`)
-              return {
-                status: 'failure',
-                data: {
-                  id: id,
-                },
-              }
+        }
+        allTagJson {
+          edges {
+            node {
+              prefix
+              name
             }
           }
+        }
+      }
+    `,
+  )
 
-          // Download file from cache if cache does not exist
-          if (!fs.existsSync('.tmp/crawler.json')) {
-            reporter.info(`Downloading prefetched data from GitHub Gist`)
-            const gistCache = await axios.get(PREFETCH_GIST)
+  if (result.errors) {
+    console.error(result.errors)
+    throw result.errors
+  }
 
-            await fs.writeFile(`.tmp/crawler.json`, JSON.stringify(gistCache.data), function(err) {
-              if (err) {
-                console.log(err)
-                reject(err)
-              }
-            })
-          } else {
-            reporter.info(`Found cache! Skipping prefetch stage`)
+  /**
+   * Functions for query data from NHentai API
+   * @param {int} id Gallery ID
+   */
+  const getRawData = async id => {
+    try {
+      let isCacheFound = false
+      let cacheRes
+      // Read file from cache
+      if (fs.existsSync('.tmp/crawler.json')) {
+        const reader = fs.readFileSync('.tmp/crawler.json', 'utf8')
+        const objects = JSON.parse(reader)
+
+        _.each(objects, object => {
+          if (object.data.id === id && object.status === 'success') {
+            isCacheFound = true
+            cacheRes = object
           }
-
-          // Begin to fetch data
-          const queue = new TaskQueue(Promise, MAX_SIMULTANEOUS_DOWNLOADS)
-          const res = {}
-          res.tags = result.data.allTagJson
-          res.data = await Promise.all(
-            result.data.allDataJson.edges.map(queue.wrap(async edge => await getRawData(edge.node.nh_id))),
-          )
-
-          return res
         })
-        .then(result => {
-          /**
-           * Filter errors and assign contants
-           */
-          const healthyResults = _.filter(result.data, o => o.status === 'success')
-          const tagStack = result.tags.edges
+      }
 
-          /**
-           * Create listing page
-           */
-          createPage({
-            path: `listing`,
-            component: path.resolve('./src/templates/listing.js'),
-            context: {
-              subtitle: `listing`,
-              raw: healthyResults.reverse(),
-            },
-          })
+      if (isCacheFound) {
+        return cacheRes
+      } else {
+        // Using reverse proxy server to avoid CORS issue
+        const out = await axios.get(`https://nh-express-git-master.rayriffy.now.sh/api/gallery/${id}`)
+        return {
+          status: 'success',
+          data: {
+            id: id,
+            raw: out.data,
+          },
+        }
+      }
+    } catch (err) {
+      reporter.warn(`cannot process ${id} with code ${err.code}`)
+      return {
+        status: 'failure',
+        data: {
+          id: id,
+        },
+      }
+    }
+  }
 
-          /**
-           * Create gallery pages
-           */
-          const postPrefix = 'r'
-          _.each(healthyResults, node => {
-            createPage({
-              path: `${postPrefix}/${node.data.id}`,
-              component: path.resolve('./src/templates/post.js'),
-              context: {
-                id: node.data.id,
-                raw: node.data.raw,
-              },
-            })
-          })
+  // Download file from cache if cache does not exist
+  if (!fs.existsSync('.tmp/crawler.json')) {
+    reporter.info(`Downloading prefetched data from GitHub Gist`)
+    const gistCache = await axios.get(PREFETCH_GIST)
 
-          /**
-           * Filter only tags object with specified types
-           * @param {object} nodes  healthyTags
-           * @param {string} type   Tag type
-           */
-          const tagFilter = (nodes, type) => {
-            const res = []
-            _.each(nodes, node => {
-              _.each(node.data.raw.tags, tag => {
-                if (tag.type === type) {
-                  if (_.isEmpty(_.filter(res, node => node.id === tag.id))) {
-                    res.push(tag)
-                  }
-                }
-              })
-            })
-            return res
+    await fs.writeFile(`.tmp/crawler.json`, JSON.stringify(gistCache.data), function(err) {
+      if (err) {
+        console.log(err)
+        throw err
+      }
+    })
+  } else {
+    reporter.info(`Found cache! Skipping prefetch stage`)
+  }
+
+  // Begin to fetch data
+  const queue = new TaskQueue(Promise, MAX_SIMULTANEOUS_DOWNLOADS)
+  const fetchedData = {}
+  fetchedData.tags = result.data.allTagJson
+  fetchedData.data = await Promise.all(
+    result.data.allDataJson.edges.map(queue.wrap(async edge => await getRawData(edge.node.nh_id))),
+  )
+
+  /**
+   * Filter errors and assign contants
+   */
+  const healthyResults = _.filter(fetchedData.data, o => o.status === 'success')
+  const tagStack = fetchedData.tags.edges
+
+  /**
+   * Create listing page
+   */
+  createPage({
+    path: `listing`,
+    component: path.resolve('./src/templates/listing.js'),
+    context: {
+      subtitle: `listing`,
+      raw: healthyResults.reverse(),
+    },
+  })
+
+  /**
+   * Create gallery pages
+   */
+  const postPrefix = 'r'
+  _.each(healthyResults, node => {
+    createPage({
+      path: `${postPrefix}/${node.data.id}`,
+      component: path.resolve('./src/templates/post.js'),
+      context: {
+        id: node.data.id,
+        raw: node.data.raw,
+      },
+    })
+  })
+
+  /**
+   * Filter only tags object with specified types
+   * @param {object} nodes  healthyTags
+   * @param {string} type   Tag type
+   */
+  const tagFilter = (nodes, type) => {
+    const res = []
+    _.each(nodes, node => {
+      _.each(node.data.raw.tags, tag => {
+        if (tag.type === type) {
+          if (_.isEmpty(_.filter(res, node => node.id === tag.id))) {
+            res.push(tag)
           }
+        }
+      })
+    })
+    return res
+  }
 
-          /**
-           * Create listing pages for each tags
-           * @param {string} pathPrefix   Tag path prefix
-           * @param {object} nodes  Filtered tag object
-           * @param {string} name        Tag name
-           */
-          const createSlugPages = (pathPrefix, name, nodes) => {
-            _.each(nodes, tag => {
-              const qualifiedResults = []
-              _.each(healthyResults, node => {
-                if (!_.isEmpty(_.filter(node.data.raw.tags, {id: tag.id}))) qualifiedResults.push(node)
-              })
-              createPage({
-                path: `${pathPrefix}/${tag.id}`,
-                component: path.resolve('./src/templates/listing.js'),
-                context: {
-                  subtitle: `${name}/${tag.name}`,
-                  raw: qualifiedResults,
-                  tagStack,
-                },
-              })
-            })
-          }
+  /**
+   * Create listing pages for each tags
+   * @param {string} pathPrefix   Tag path prefix
+   * @param {object} nodes  Filtered tag object
+   * @param {string} name        Tag name
+   */
+  const createSlugPages = (pathPrefix, name, nodes) => {
+    _.each(nodes, tag => {
+      const qualifiedResults = []
+      _.each(healthyResults, node => {
+        if (!_.isEmpty(_.filter(node.data.raw.tags, {id: tag.id}))) qualifiedResults.push(node)
+      })
+      createPage({
+        path: `${pathPrefix}/${tag.id}`,
+        component: path.resolve('./src/templates/listing.js'),
+        context: {
+          subtitle: `${name}/${tag.name}`,
+          raw: qualifiedResults,
+          tagStack,
+        },
+      })
+    })
+  }
 
-          /**
-           * Creating tag listing and pages recursively
-           */
-          _.each(tagStack, edge => {
-            const nodes = tagFilter(healthyResults, edge.node.name)
-            createSlugPages(edge.node.prefix, edge.node.name, nodes)
-            createPage({
-              path: `${edge.node.prefix}`,
-              component: path.resolve('./src/templates/tag.js'),
-              context: {
-                prefix: edge.node.prefix,
-                subtitle: `${edge.node.name}`,
-                raw: nodes,
-              },
-            })
-          })
+  /**
+   * Creating tag listing and pages recursively
+   */
+  _.each(tagStack, edge => {
+    const nodes = tagFilter(healthyResults, edge.node.name)
+    createSlugPages(edge.node.prefix, edge.node.name, nodes)
+    createPage({
+      path: `${edge.node.prefix}`,
+      component: path.resolve('./src/templates/tag.js'),
+      context: {
+        prefix: edge.node.prefix,
+        subtitle: `${edge.node.name}`,
+        raw: nodes,
+      },
+    })
+  })
 
-          /**
-           * Put all healthy results into cache
-           */
-          fs.writeFile(`.tmp/crawler.json`, JSON.stringify(healthyResults), function(err) {
-            if (err) {
-              console.log(err)
-              reject(err)
-            }
-          })
+  /**
+   * Put all healthy results into cache
+   */
+  fs.writeFile(`.tmp/crawler.json`, JSON.stringify(healthyResults), function(err) {
+    if (err) {
+      console.error(err)
+      throw err
+    }
+  })
 
-          /**
-           * Preparing to generate API
-           */
-          const apiPath = 'api'
-          const chunks = _.chunk(healthyResults, 10)
+  /**
+   * Preparing to generate API
+   */
+  const apiPath = 'api'
+  const chunks = _.chunk(healthyResults, 10)
 
-          if (!fs.existsSync('./public/api')) {
-            fs.mkdirSync('./public/api', function(err) {
-              if (err) {
-                console.log(err)
-                reject(err)
-              }
-            })
-          }
-          if (!fs.existsSync('./public/api/list')) {
-            fs.mkdirSync('./public/api/list', function(err) {
-              if (err) {
-                console.log(err)
-                reject(err)
-              }
-            })
-          }
+  if (!fs.existsSync('./public/api')) {
+    fs.mkdirSync('./public/api', function(err) {
+      if (err) {
+        console.error(err)
+        throw err
+      }
+    })
+  }
+  if (!fs.existsSync('./public/api/list')) {
+    fs.mkdirSync('./public/api/list', function(err) {
+      if (err) {
+        console.error(err)
+        throw err
+      }
+    })
+  }
 
-          /**
-           * Generate API status page
-           */
-          fs.writeFile(
-            `public/${apiPath}/status.json`,
-            JSON.stringify({
-              status: 'success',
-              code: 201,
-              data: {
-                time: Date.now(),
-                list: {
-                  length: chunks.length,
-                },
-              },
-            }),
-            function(err) {
-              if (err) {
-                console.log(err)
-                reject(err)
-              }
-            },
-          )
+  /**
+   * Generate API status page
+   */
+  fs.writeFile(
+    `public/${apiPath}/status.json`,
+    JSON.stringify({
+      status: 'success',
+      code: 201,
+      data: {
+        time: Date.now(),
+        list: {
+          length: chunks.length,
+        },
+      },
+    }),
+    function(err) {
+      if (err) {
+        console.error(err)
+        throw err
+      }
+    },
+  )
 
-          /**
-           * Generate API listing pages
-           */
-          _.each(chunks, (chunk, i) => {
-            let out = {
-              status: 'success',
-              code: 201,
-              data: [],
-            }
-            _.each(chunk, node => {
-              out.data.push(node.data.raw)
-            })
-            fs.writeFile(`public/${apiPath}/list/${i + 1}.json`, JSON.stringify(out), function(err) {
-              if (err) {
-                console.log(err)
-                reject(err)
-              }
-            })
-          })
-        }),
-    )
+  /**
+   * Generate API listing pages
+   */
+  _.each(chunks, (chunk, i) => {
+    let out = {
+      status: 'success',
+      code: 201,
+      data: [],
+    }
+    _.each(chunk, node => {
+      out.data.push(node.data.raw)
+    })
+    fs.writeFile(`public/${apiPath}/list/${i + 1}.json`, JSON.stringify(out), function(err) {
+      if (err) {
+        console.error(err)
+        throw err
+      }
+    })
   })
 }
 
