8ca6c10cb3011f9c5b296af21f0157b70bc788d2
Phumrapee Limpianchop <git@rayriffy.com>
2019-09-18 23:07:12 +0700

    :art: 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,23 +1,41 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 
-import { Box, Card, Flex, Text } from 'rebass'
+import { Box, Card, Flex, Image, Text } from 'rebass'
 import styled from 'styled-components'
+
+import { Subtitle } from '../app/context'
 
 const BorderedCard = styled(Card)`
   border-radius: 8px;
   border: 1px solid #e8e8e8;
+  overflow: hidden;
 
   background: #fff;
 `
 
+const WidthImage = styled(Image)`
+  width: 100%
+`
+
 const NotFoundComponent: React.FC = () => {
+  const [, setSubtitle] = useContext(Subtitle)
+
+  useEffect(() => {
+    if (setSubtitle) {
+      setSubtitle('404')
+    }
+  }, [])
+
   return (
-    <Box py={3}>
+    <Box pt={3}>
       <Flex justifyContent={`center`}>
-        <Box>
-          <BorderedCard p={4}>
-            <Text fontSize={58} fontWeight={700}>404</Text>
-            <Text fontSize={24} fontWeight={600}>Not Found</Text>
+        <Box width={[20 / 24, 16 / 24, 12 / 24, 8 / 24]}>
+          <BorderedCard>
+            <WidthImage src={`https://media.giphy.com/media/uS1hYCwTrW3ks/giphy.gif`} />
+            <Box px={3} pt={1} pb={3}>
+              <Text fontSize={[18, 20, 22, 24]} fontWeight={600} pt={2}>Not found</Text>
+              <Text fontSize={[14, 15]} pt={2}>The page your're looking for is not found</Text>
+            </Box>
           </BorderedCard>
         </Box>
       </Flex>
