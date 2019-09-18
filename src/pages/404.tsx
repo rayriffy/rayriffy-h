e89aaa1c81981aa4fc92997c839c967e0677a138
Phumrapee Limpianchop <git@rayriffy.com>
2019-09-18 16:32:39 +0700

    :art: 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
 
-const NotFoundComponent: React.FC = props => {
+import { Box, Card, Flex, Text } from 'rebass'
+import styled from 'styled-components'
+
+const BorderedCard = styled(Card)`
+  border-radius: 8px;
+  border: 1px solid #e8e8e8;
+
+  background: #fff;
+`
+
+const NotFoundComponent: React.FC = () => {
   return (
-    <>Not Found</>
+    <Box py={3}>
+      <Flex justifyContent={`center`}>
+        <Box>
+          <BorderedCard p={4}>
+            <Text fontSize={58} fontWeight={700}>404</Text>
+            <Text fontSize={24} fontWeight={600}>Not Found</Text>
+          </BorderedCard>
+        </Box>
+      </Flex>
+    </Box>
   )
 }
 
