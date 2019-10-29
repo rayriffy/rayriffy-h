8669e8ab706faa319a4be6b304cbac745f093b34
Phumrapee Limpianchop <git@rayriffy.com>
2019-10-29 21:59:49 +0700

    :construction: WIP search by split words

--- src/templates/search/services/searchHentai.ts
+++ src/templates/search/services/searchHentai.ts
@@ -3,33 +3,41 @@ import _ from 'lodash'
 import { IFetchedRaw } from '../../../core/@types/IFetchedRaw'
 
 export const searchHentai = async (query: string, raws: IFetchedRaw[]): Promise<IFetchedRaw[]> => {
-  /**
-   * Filter the search by names
-   */
-
-  const languages = ['english', 'japanese', 'pretty']
-
-  const typeName: IFetchedRaw[] = _.flatten(_.union(languages.map(language => {
-    return raws.filter(raw => {
-      const title: string = _.get(raw, `title.${language}`, '').toLocaleLowerCase()
-
-      return RegExp(query.toLocaleLowerCase()).test(title)
-    })
-  })))
-
-  /**
-   * Filter the search by tags
-   */
-
-  const typeTag: IFetchedRaw[] = raws.filter(raw => {
-    const tagResult: boolean[] = raw.data.raw.tags.map(tag => {
-      const name = tag.name.toLocaleLowerCase()
-
-      return RegExp(query.toLocaleLowerCase()).test(name)
-    })
-
-    return tagResult.some(o => o === true)
+  const resultsByWords = query.split(' ').map(subquery => {
+    if (subquery === '') {
+      return []
+    } else {
+      /**
+       * Filter the search by names
+       */
+    
+      const languages = ['english', 'japanese', 'pretty']
+    
+      const typeName: IFetchedRaw[] = _.flatten(_.union(languages.map(language => {
+        return raws.filter(raw => {
+          const title: string = _.get(raw, `title.${language}`, '').toLocaleLowerCase()
+    
+          return RegExp(subquery.toLocaleLowerCase()).test(title)
+        })
+      })))
+    
+      /**
+       * Filter the search by tags
+       */
+    
+      const typeTag: IFetchedRaw[] = raws.filter(raw => {
+        const tagResult: boolean[] = raw.data.raw.tags.map(tag => {
+          const name = tag.name.toLocaleLowerCase()
+    
+          return RegExp(subquery.toLocaleLowerCase()).test(name)
+        })
+    
+        return tagResult.some(o => o === true)
+      })
+    
+      return _.union(typeName, typeTag)
+    }
   })
 
-  return _.union(typeName, typeTag)
+  return _.intersection(_.flatten(resultsByWords))
 }
