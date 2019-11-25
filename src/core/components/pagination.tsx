60ed4d8e077338b20653071fe9918c63d1b314eb
Phumrapee Limpianchop <git@rayriffy.com>
2019-11-25 22:04:27 +0700

    :zap: Pagination optimization

--- src/core/components/pagination.tsx
+++ src/core/components/pagination.tsx
@@ -1,10 +1,39 @@
 import React from 'react'
 
-import { Box, Flex, Link } from '@chakra-ui/core'
+import { Box, Flex, theme } from '@chakra-ui/core'
+import styled from '@emotion/styled'
 
-import { IPaginationProps } from '../@types/IPaginationProps'
+import Link from '../../core/components/transparentLink'
 
-const PaginationComponent: React.FC<IPaginationProps> = props => {
+interface IProps {
+  max: number
+  current: number
+  prefix: string
+}
+
+interface IPage {
+  start: number
+  index: number
+  current: number
+}
+
+const TransparentLink = styled(Link)<IPage>`
+  text-decoration: none;
+
+  ${(props: IPage) => {
+    const { start, index, current } = props
+
+    const themeColor: any = theme.colors
+
+    if (start + index + 1 === current) {
+      return `color: ${themeColor.black};`
+    } else {
+      return `color: ${themeColor.gray[500]};`
+    }
+  }}
+`
+
+const PaginationComponent: React.FC<IProps> = props => {
   const { max, current, prefix } = props
 
   const pageLength: number = max > 5 ? 5 : max
@@ -21,14 +50,15 @@ const PaginationComponent: React.FC<IPaginationProps> = props => {
     <Flex justifyContent='center'>
       {Array.from({ length: pageLength }, (_, i) => (
         <Box key={`pagination-${startPoint + i}`} px={3}>
-          <Link
-            href={
+          <TransparentLink
+            to={
               startPoint + i === 0 ? prefix : `${prefix}p/${startPoint + i + 1}`
             }
-            _hover={{ textDecoration: 'none' }}
-            color={startPoint + i + 1 === current ? 'black' : 'gray.500'}>
+            start={startPoint}
+            index={i}
+            current={current}>
             {startPoint + i + 1}
-          </Link>
+          </TransparentLink>
         </Box>
       ))}
     </Flex>
