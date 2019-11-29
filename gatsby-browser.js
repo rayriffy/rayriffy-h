d6153027d86a340d0fce0596eedd7f90f8e2f933
Phumrapee Limpianchop <git@rayriffy.com>
2019-11-30 00:39:24 +0700

    :art: UX Improvements

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -19,6 +19,10 @@ export const onServiceWorkerUpdateFound = () => {
 export const onServiceWorkerInstalled = () => {
   document.getElementById('sw-update-found').style.display = 'none'
   document.getElementById('sw-update-installed').style.display = 'flex'
+
+  setTimeout(() => {
+    document.getElementById('sw-update-installed').style.display = 'none'
+  }, 5000)
 }
 
 export const onServiceWorkerUpdateReady = () => {
