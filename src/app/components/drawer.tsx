450a655fa472cedd0084c99671a86d7c1dc8f13d
Phumrapee Limpianchop <git@rayriffy.com>
2019-12-09 01:09:48 +0700

    :package: Version bump

--- src/app/components/drawer.tsx
+++ src/app/components/drawer.tsx
@@ -100,7 +100,7 @@ const DrawerComponent: React.FC = () => {
               <Heading size='xl'>Riffy H</Heading>
               <Text color='gray.500'>The missng piece of NHentai</Text>
               <Text color='gray.400' fontSize='xs'>
-                Version 3.2.1
+                Version 3.3.0
               </Text>
             </Box>
             <Divider mt={4} mb={2} />
