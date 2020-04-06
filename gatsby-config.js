fc3bd136f2ab5e862b427e2eac6d4c22787849ca
Phumrapee Limpianchop <git@rayriffy.com>
2020-04-06 20:54:13 +0700

    chore: staged changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,2 +1,5 @@
+// TS Node is added to support TypeScript functionality.
+// Important! Register ts-node only once since it will cause complier duplication
 require('ts-node').register()
+
 module.exports = require('./src/gatsby/config')
