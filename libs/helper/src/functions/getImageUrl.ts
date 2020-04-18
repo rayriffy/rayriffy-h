d69b25921e3a763c7b05687a1cf4973b70088f92
Phumrapee Limpianchop <git@rayriffy.com>
2020-04-18 18:13:22 +0700

    fix(helper): thumbnail url decode

--- libs/helper/src/functions/getImageUrl.ts
+++ libs/helper/src/functions/getImageUrl.ts
@@ -10,5 +10,5 @@ export interface GetImageUrlArgs {
 export const getImageUrl = (args: GetImageUrlArgs): string => {
   const { image, type, mediaId, page } = args
 
-  return `https://${type === 'cover' ? 't' : 'i'}.nhentai.net/galleries/${mediaId}/${type === 'cover' ? 'cover' : page}.${image.t === 'p' ? 'png' : image.t === 'g' ? 'gif' : 'jpg'}`
+  return `https://${type === 'gallery'? 'i' : 't'}.nhentai.net/galleries/${mediaId}/${type === 'cover' ? 'cover' : `${page}${type === 'thumbnail' ? 't' : ''}`}.${image.t === 'p' ? 'png' : image.t === 'g' ? 'gif' : 'jpg'}`
 }
