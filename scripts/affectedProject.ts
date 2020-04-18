8605a4a0674f8ae3fe8c124469f4b3acd169a7e8
Phumrapee Limpianchop <git@rayriffy.com>
2020-04-18 19:45:13 +0700

    ci: update app

--- scripts/affectedProject.ts
+++ scripts/affectedProject.ts
@@ -1,12 +1,20 @@
 import { program } from 'commander'
 import { execSync } from 'child_process'
 
-program.option('-p, --project <name>', 'specified project name')
+program
+  .option('-p, --project <name>', 'specified project name')
+  .option('--list', 'print affected projects', false)
+  .option('--base <commit>', 'base commit to compare', 'HEAD~1')
+  .option('--head <commit>', 'head commit to compare', `HEAD`)
 
 program.parse(process.argv)
 
-const { project } = program
+const { list, project, base, head } = program
 
-const printAffected = JSON.parse(execSync(`yarn --silent nx print-affected --base=HEAD~1 --head=HEAD`).toString())
+const printAffected = JSON.parse(execSync(`yarn --silent nx print-affected --base=${base} --head=${head}`).toString())
 
-console.log(printAffected.projects.includes(project))
+if (list) {
+  printAffected.projects.map(o => console.log(o))
+} else {
+  console.log(printAffected.projects.includes(project))
+}
