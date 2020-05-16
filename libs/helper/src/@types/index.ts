cbfc9dbf06be91cfa45cc1a3a922027bd68702f9
Phumrapee Limpianchop <git@rayriffy.com>
2020-05-16 22:08:26 +0700

    Revert "feat: tree-shake"
    This reverts commit c68f9512b8b2cff94dfe401018aba9139dbab345.

---
+++ libs/helper/src/@types/index.ts
@@ -0,0 +1,7 @@
+export * from './APIResponse'
+export * from './Hentai'
+export * from './Image'
+export * from './RawHentai'
+export * from './RawTag'
+export * from './Tag'
+export * from './TagType'
