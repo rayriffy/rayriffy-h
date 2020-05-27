8c3b0ba81fc8474ee0cccc2bd34b74940516b0da
Phumrapee Limpianchop <git@rayriffy.com>
2020-05-27 21:06:32 +0700

    refactor: use fontawesome

--- apps/web/src/app/components/navigation.tsx
+++ apps/web/src/app/components/navigation.tsx
@@ -21,7 +21,7 @@ const Dropdown: React.FC<{ title: React.ReactNode, zIndex?: number }> = props =>
     <div className="relative">
       <button onClick={() => setOpen(o => !o)} className="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-transparent rounded-lg dark:hover:bg-gray-600 dark:focus:bg-gray-600 dark:focus:text-white dark:hover:text-white dark:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
         <span>{title}</span>
-        <svg fill="currentColor" viewBox="0 0 20 20" className={`inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform ${open ? 'rotate-180' : 'rotate-0' }`}><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+        <i className={`ml-2 fas fa-angle-${open ? 'up' : 'down'}`}></i>
       </button>
       <div className={`absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg transition-all duration-200 ${open ? `opacity-100 z-${zIndex}` : 'opacity-0 z-hide'}`}>
         <div className={`px-2 py-2 bg-white rounded-md shadow-2xl dark:bg-gray-800 ${open ? 'block' : 'hidden'}`}>
