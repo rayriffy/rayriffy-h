bff7822294fe13e00725a474a9e0a122c2e1fac2
Phumrapee Limpianchop <git@rayriffy.com>
2020-05-29 22:07:41 +0700

    fix: service worker notification

--- apps/web/src/app/components/serviceworker.tsx
+++ apps/web/src/app/components/serviceworker.tsx
@@ -10,8 +10,8 @@ export const ServiceWorker: React.FC = () => {
         </div>
         <div id='sw-update-green' className='p-2 bg-green-800 items-center text-green-100 leading-none rounded-full mb-4 hidden' role='alert'>
           <span className='flex rounded-full bg-green-500 uppercase px-2 py-1 text-xs font-bold mr-3'>New</span>
-          <span id='sw-update-complete' className='font-semibold mr-2 text-left flex-auto'>Update complete! Reload required.</span>
-          <span id='sw-update-installed' className='font-semibold mr-2 text-left flex-auto'>Ready to work offline!</span>
+          <span id='sw-update-complete' className='font-semibold mr-2 text-left flex-auto hidden'>Update complete! Reload required.</span>
+          <span id='sw-update-installed' className='font-semibold mr-2 text-left flex-auto hidden'>Ready to work offline!</span>
         </div>
       </div>
     </React.Fragment>
