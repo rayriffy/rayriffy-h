62eb0df18a5a92fca1a1421981a5daeab43b3972
Phumrapee Limpianchop <git@rayriffy.com>
2020-06-01 21:47:06 +0700

    style: cssnano

--- apps/web/postcss.config.js
+++ apps/web/postcss.config.js
@@ -3,6 +3,8 @@
 module.exports = () => ({
   plugins: [
     require('tailwindcss')('./tailwind.config.js'),
-    require('autoprefixer'),
+    require('cssnano')({
+      preset: 'advanced',
+    }),
   ],
 })
