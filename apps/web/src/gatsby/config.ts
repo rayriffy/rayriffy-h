2cb901e986c82bccf82cd6eca2180ae215483406
Phumrapee Limpianchop <git@rayriffy.com>
2020-08-09 12:54:17 +0900

    chore: staged changes

--- apps/web/src/gatsby/config.ts
+++ apps/web/src/gatsby/config.ts
@@ -77,24 +77,29 @@ const config: GatsbyConfig = {
       options: {
         runtimeCaching: [
           {
+            // Use cacheFirst since these don't need to be revalidated (same RegExp
+            // and same reason as above)
             urlPattern: /(\.js$|\.css$|static\/)/,
-            handler: `cacheFirst`,
+            handler: `CacheFirst`,
           },
           {
+            // page-data.json files are not content hashed
             urlPattern: /^https?:.*\page-data\/.*\/page-data\.json/,
-            handler: `networkFirst`,
+            handler: `NetworkFirst`,
           },
           {
+            // Add runtime caching of various other page resources
             urlPattern: /^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
-            handler: `staleWhileRevalidate`,
+            handler: `StaleWhileRevalidate`,
           },
           {
+            // Google Fonts CSS (doesn't end in .css so we need to specify it)
             urlPattern: /^https?:\/\/fonts\.googleapis\.com\/css/,
-            handler: `staleWhileRevalidate`,
+            handler: `StaleWhileRevalidate`,
           },
           {
-            urlPattern: /^https?:\/\/h\.api\.rayriffy\.com\/v1\/gallery/,
-            handler: `staleWhileRevalidate`,
+            urlPattern: /^https?:\/\/h\.api\.rayriffy\.com/,
+            handler: `NetworkFirst`,
           },
         ],
         skipWaiting: true,
