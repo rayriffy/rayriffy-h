131734a793399ffae1d6221721dbff3bf5c9c2cc
Phumrapee Limpianchop <git@rayriffy.com>
2020-10-29 23:30:18 +0900

    feat(web-next): 404

---
+++ apps/web-next/src/core/components/mediaCard.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+interface IProps {
+  media: string
+  title: string
+  subtitle: string
+}
+
+export const MediaCard: React.FC<IProps> = props => {
+  const { media, title, subtitle } = props
+
+  return (
+    <div className="bg-white overflow-hidden shadow rounded-lg">
+      <video src={media} loop autoPlay className="w-full h-auto" />
+      <div className="px-4 py-4 sm:px-6">
+        <h1 className="font-bold text-gray-800 text-xl">{title}</h1>
+        <p className="text-gray-500">{subtitle}</p>
+      </div>
+    </div>
+  )
+}
