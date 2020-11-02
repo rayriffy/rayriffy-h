d1c868dfa5c4c7ab9c315b67f37cbdc6d39ccecc
Phumrapee Limpianchop <git@rayriffy.com>
2020-11-03 01:12:05 +0900

    fix: media overflow

--- apps/web-next/src/core/components/mediaCard.tsx
+++ apps/web-next/src/core/components/mediaCard.tsx
@@ -11,7 +11,7 @@ export const MediaCard: React.FC<IProps> = React.memo(props => {
 
   return (
     <div className="bg-white overflow-hidden shadow rounded-lg">
-      <video src={media} loop autoPlay className="w-full h-auto" />
+      <video src={media} loop autoPlay className="w-full h-auto rounded-t-lg" />
       <div className="px-4 py-4 sm:px-6">
         <h1 className="font-bold text-gray-800 text-xl">{title}</h1>
         <p className="text-gray-500">{subtitle}</p>
