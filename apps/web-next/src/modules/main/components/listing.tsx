39fbdff5320ad950a75b53e7a9ba2c2d0c8049d7
Phumrapee Limpianchop <git@rayriffy.com>
2020-11-04 02:05:35 +0900

    refactor: staged changes

--- apps/web-next/src/modules/main/components/listing.tsx
+++ apps/web-next/src/modules/main/components/listing.tsx
@@ -18,69 +18,71 @@ export const MainListing: React.FC<IProps> = props => {
 
   console.log('data: ', data)
 
-  return (
-    <div className="p-2 sm:p-4">
-      {isError ? (
-        <div className="pt-16 max-w-xl mx-auto">
-          <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100">
-            <Exclamation className="h-6 w-6 text-red-600" />
-          </div>
-          <div className="pt-4">
-            <p className="font-bold text-lg text-gray-800 text-center">
-              Failed
-            </p>
-            <p className="text-sm text-gray-800 text-center">
-              This error could caused by NHentai API itself and it usually take
-              a few hours to be fixed. In meanwhile, you can explore other
-              galleries in "Listing" section
-            </p>
-          </div>
+  if (isError) {
+    return (
+      <div className="pt-16 max-w-xl mx-auto">
+        <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100">
+          <Exclamation className="h-6 w-6 text-red-600" />
         </div>
-      ) : isLoading ? (
-        <div className="pt-16">
-          <div className="flex justify-center">
-            <div className="w-8 h-8 spinner border-2" />
-          </div>
-          <div className="pt-4">
-            <p className="font-bold text-lg text-gray-800 text-center">
-              Obtaining data
-            </p>
-            <p className="text-sm text-gray-800 text-center">
-              This should take only few seconds...
-            </p>
-          </div>
+        <div className="pt-4">
+          <p className="font-bold text-lg text-gray-800 text-center">Failed</p>
+          <p className="text-sm text-gray-800 text-center">
+            This error could caused by NHentai API itself and it usually take a
+            few hours to be fixed. In meanwhile, you can explore other galleries
+            in "Listing" section
+          </p>
         </div>
-      ) : data.galleries.length === 0 ? (
-        <div className="pt-16 max-w-xl mx-auto">
-          <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-orange-100">
-            <QuestionMarkCircle className="h-6 w-6 text-orange-600" />
-          </div>
-          <div className="pt-4">
-            <p className="font-bold text-lg text-gray-800 text-center">
-              Not Found
-            </p>
-            <p className="text-sm text-gray-800 text-center">
-              No results for this search query / page
-            </p>
-          </div>
+      </div>
+    )
+  } else if (isLoading) {
+    return (
+      <div className="pt-16">
+        <div className="flex justify-center">
+          <div className="w-8 h-8 spinner border-2" />
         </div>
-      ) : (
-        <React.Fragment>
-          <div className="flex justify-center pt-4">
-            <nav className="z-0">
-              <Pagination max={data.maxPage} current={page} prefix="/" />
-            </nav>
-          </div>
-          <div className="py-4">
-            <Listing {...{ galleries: data.galleries }} />
-          </div>
-          <div className="flex justify-center pb-4">
-            <nav className="z-0">
-              <Pagination max={data.maxPage} current={page} prefix="/" />
-            </nav>
-          </div>
-        </React.Fragment>
-      )}
-    </div>
-  )
+        <div className="pt-4">
+          <p className="font-bold text-lg text-gray-800 text-center">
+            Obtaining data
+          </p>
+          <p className="text-sm text-gray-800 text-center">
+            This should take only few seconds...
+          </p>
+        </div>
+      </div>
+    )
+  } else if (data.galleries.length === 0) {
+    return (
+      <div className="pt-16 max-w-xl mx-auto">
+        <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-orange-100">
+          <QuestionMarkCircle className="h-6 w-6 text-orange-600" />
+        </div>
+        <div className="pt-4">
+          <p className="font-bold text-lg text-gray-800 text-center">
+            Not Found
+          </p>
+          <p className="text-sm text-gray-800 text-center">
+            No results for this search query / page
+          </p>
+        </div>
+      </div>
+    )
+  } else {
+    return (
+      <div className="p-2 sm:p-4">
+        <div className="flex justify-center pt-4">
+          <nav className="z-0">
+            <Pagination max={data.maxPage} current={page} prefix="/" />
+          </nav>
+        </div>
+        <div className="py-4">
+          <Listing {...{ galleries: data.galleries }} />
+        </div>
+        <div className="flex justify-center pb-4">
+          <nav className="z-0">
+            <Pagination max={data.maxPage} current={page} prefix="/" />
+          </nav>
+        </div>
+      </div>
+    )
+  }
 }
