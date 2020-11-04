564c8151e6caf0e5b755d41b16b57df553a29a3e
Phumrapee Limpianchop <git@rayriffy.com>
2020-11-05 05:35:13 +0900

    chore: attempt 2

--- apps/web-next/postcss.config.js
+++ apps/web-next/postcss.config.js
@@ -14,6 +14,7 @@ module.exports = {
       {
         content: [
           './src/**/*.{js,jsx,ts,tsx}',
+          './apps/web-next/src/**/*.{js,jsx,ts,tsx}',
           // './components/**/*.{js,jsx,ts,tsx}',
         ],
         defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
