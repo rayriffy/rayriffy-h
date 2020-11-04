ebf74b25aa03333c3f0f67ec54f569a8f6bf422a
Phumrapee Limpianchop <git@rayriffy.com>
2020-11-05 05:47:06 +0900

    chore: safelist

--- apps/web-next/postcss.config.js
+++ apps/web-next/postcss.config.js
@@ -15,8 +15,11 @@ module.exports = {
         content: [
           './src/**/*.{js,jsx,ts,tsx}',
           './apps/web-next/src/**/*.{js,jsx,ts,tsx}',
+          './libs/datasource/src/**/*.{js,jsx,ts,tsx}',
+          './libs/icons/src/**/*.{js,jsx,ts,tsx}',
           // './components/**/*.{js,jsx,ts,tsx}',
         ],
+        safelist: ['html', 'body'],
         defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
       },
     ],
