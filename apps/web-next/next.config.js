f88687d48418c071a5a87dcef341de0b69490a33
Phumrapee Limpianchop <git@rayriffy.com>
2020-11-13 06:16:01 +0900

    feat: preact

--- apps/web-next/next.config.js
+++ apps/web-next/next.config.js
@@ -4,59 +4,14 @@ const moment = require('moment-timezone')
 const withPlugins = require('next-compose-plugins')
 
 const withWorkers = require('@zeit/next-workers')
-const withPrefresh = require('@prefresh/next')
 const withOffline = require('next-offline')
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
-
-const withPreact = (nextConfig = {}) => {
-  return Object.assign({}, nextConfig, {
-    webpack(config, { dev, isServer }) {
-      // Move Preact into the framework chunk instead of duplicating in routes:
-      const splitChunks = config.optimization && config.optimization.splitChunks
-      if (splitChunks) {
-        const cacheGroups = splitChunks.cacheGroups
-        const test = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/
-        if (cacheGroups.framework) {
-          cacheGroups.preact = Object.assign({}, cacheGroups.framework, {
-            test,
-          })
-          // if you want to merge the 2 small commons+framework chunks:
-          // cacheGroups.commons.name = 'framework';
-        }
-      }
-
-      if (isServer) {
-        // mark `preact` stuffs as external for server bundle to prevent duplicate copies of preact
-        config.externals.push(
-          /^(preact|preact-render-to-string|preact-context-provider)([\\/]|$)/
-        )
-      }
-
-      // Install webpack aliases:
-      const aliases = config.resolve.alias || (config.resolve.alias = {})
-      aliases.react = aliases['react-dom'] = 'preact/compat'
-
-      // Automatically inject Preact DevTools:
-      if (dev && !isServer) {
-        const entry = config.entry
-        config.entry = () =>
-          entry().then(entries => {
-            entries['main.js'] = ['preact/debug'].concat(
-              entries['main.js'] || []
-            )
-            return entries
-          })
-      }
-
-      return config
-    },
-  })
-}
+const withPreact = require('next-plugin-preact')
 
 module.exports = withPlugins(
-  [[withOffline], [withWorkers], [withBundleAnalyzer]],
+  [[withOffline], [withWorkers], [withPreact], [withBundleAnalyzer]],
   {
     target: 'serverless',
     env: {
