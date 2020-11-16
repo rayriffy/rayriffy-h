18f6b22c80a516be17066b4eb20e2141c1b83679
Phumrapee Limpianchop <git@rayriffy.com>
2020-11-17 08:46:01 +0900

    chore: staged changes

--- apps/web-next/postcss.config.js
+++ apps/web-next/postcss.config.js
@@ -9,24 +9,19 @@ module.exports = {
         preset: 'advanced',
       },
     ],
-    ...(process.env.NODE_ENV === 'production'
-      ? [
-          [
-            '@fullhuman/postcss-purgecss',
-            {
-              content: [
-                './src/**/*.{js,jsx,ts,tsx}',
-                './apps/web-next/src/**/*.{js,jsx,ts,tsx}',
-                './libs/datasource/src/**/*.{js,jsx,ts,tsx}',
-                './libs/icons/src/**/*.{js,jsx,ts,tsx}',
-                // './components/**/*.{js,jsx,ts,tsx}',
-              ],
-              safelist: ['html', 'body'],
-              defaultExtractor: content =>
-                content.match(/[\w-/:.%]+(?<!:)/g) || [],
-            },
-          ],
-        ]
-      : []),
+    [
+      '@fullhuman/postcss-purgecss',
+      {
+        content: [
+          './src/**/*.{js,jsx,ts,tsx}',
+          './apps/web-next/src/**/*.{js,jsx,ts,tsx}',
+          './libs/datasource/src/**/*.{js,jsx,ts,tsx}',
+          './libs/icons/src/**/*.{js,jsx,ts,tsx}',
+          // './components/**/*.{js,jsx,ts,tsx}',
+        ],
+        safelist: ['html', 'body'],
+        defaultExtractor: content => content.match(/[\w-/:.%]+(?<!:)/g) || [],
+      },
+    ],
   ],
 }
