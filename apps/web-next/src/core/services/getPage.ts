32234c91485c83aec878192f69279226d4d3331c
Phumrapee Limpianchop <git@rayriffy.com>
2020-11-27 05:50:15 +0900

    fix: reverse

--- apps/web-next/src/core/services/getPage.ts
+++ apps/web-next/src/core/services/getPage.ts
@@ -3,8 +3,9 @@ import { codes } from '@rayriffy-h/datasource'
 
 import chunk from 'lodash/chunk'
 import get from 'lodash/get'
+import reverse from 'lodash/reverse'
 
 export const getPage = (page: number) => {
-  const chunkedCodes = chunk(codes, itemsPerPage)
+  const chunkedCodes = reverse(chunk(codes, itemsPerPage))
   return get(chunkedCodes, page - 1)
 }
