5514d53cba8c5631511bc0e733ff34ee89ba0b8f
Phumrapee Limpianchop <git@rayriffy.com>
2020-12-01 06:28:53 +0900

    chore: add more data

--- libs/datasource/src/codes/index.ts
+++ libs/datasource/src/codes/index.ts
@@ -1,7 +1,10 @@
 import { original } from './original'
 
 import { data2020_11 } from './months/2020-11'
+import { data2020_12 } from './months/2020-12'
 
 import { DatabaseCode } from '../@types/DatabaseCode'
 
-export const codes: DatabaseCode[] = original.concat(data2020_11)
+export const codes: DatabaseCode[] = original
+  .concat(data2020_11)
+  .concat(data2020_12)
---
+++ libs/datasource/src/codes/months/2020-12.ts
@@ -0,0 +1,12 @@
+import { DatabaseCode } from '../../@types/DatabaseCode'
+
+export const data2020_12: DatabaseCode[] = [
+  337695,
+  337764,
+  337779,
+  337822,
+  337837,
+  337912,
+  337889,
+  337914,
+]
