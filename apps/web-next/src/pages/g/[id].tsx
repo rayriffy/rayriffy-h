2e5a27975973b2527e583fe97412983f0eb39864
Phumrapee Limpianchop <git@rayriffy.com>
2021-01-09 23:15:50 +0900

    chore: getServerSideProps

--- apps/web-next/src/pages/g/[id].tsx
+++ apps/web-next/src/pages/g/[id].tsx
@@ -2,8 +2,7 @@ import React from 'react'
 
 import { getHentai, Hentai } from '@rayriffy-h/helper'
 
-import { GetStaticPaths, GetStaticProps, NextPage } from 'next'
-import { useRouter } from 'next/router'
+import { GetServerSideProps, NextPage } from 'next'
 
 import { Reader } from '../../core/components/reader'
 import { HeadTitle } from '../../core/components/headTitle'
@@ -17,47 +16,27 @@ interface IProps {
 const Page: NextPage<IProps> = props => {
   const { gallery, excludes } = props
 
-  const router = useRouter()
-
-  if (router.isFallback) {
-    return (
-      <div className="pt-16">
-        <div className="flex justify-center">
-          <div className="w-8 h-8 spinner border-2" />
-        </div>
-        <div className="pt-2">
-          <p className="font-bold text-lg text-gray-800 text-center">
-            Obtaining data
-          </p>
-          <p className="text-sm text-gray-800 text-center">
-            This should take only few seconds...
-          </p>
-        </div>
-      </div>
-    )
-  } else {
-    return (
-      <React.Fragment>
-        <HeadTitle
-          title={gallery.title.pretty}
-          description={`Read ${gallery.title.pretty} without ads or popups via Riffy H, an alternate client for nhentai`}
-        >
-          <meta
-            property="og:image"
-            content={`https://h.api.rayriffy.com/v1/og/${gallery.id}`}
-          />
-          <meta
-            property="twitter:image"
-            content={`https://h.api.rayriffy.com/v1/og/${gallery.id}`}
-          />
-        </HeadTitle>
-        <Reader {...{ hentai: gallery, excludes }} />
-      </React.Fragment>
-    )
-  }
+  return (
+    <React.Fragment>
+      <HeadTitle
+        title={gallery.title.pretty}
+        description={`Read ${gallery.title.pretty} without ads or popups via Riffy H, an alternate client for nhentai`}
+      >
+        <meta
+          property="og:image"
+          content={`https://h.api.rayriffy.com/v1/og/${gallery.id}`}
+        />
+        <meta
+          property="twitter:image"
+          content={`https://h.api.rayriffy.com/v1/og/${gallery.id}`}
+        />
+      </HeadTitle>
+      <Reader {...{ hentai: gallery, excludes }} />
+    </React.Fragment>
+  )
 }
 
-export const getStaticProps: GetStaticProps = async context => {
+export const getServerSideProps: GetServerSideProps<IProps> = async context => {
   const { codes } = await import('@rayriffy-h/datasource')
 
   try {
@@ -83,13 +62,6 @@ export const getStaticProps: GetStaticProps = async context => {
     return {
       notFound: true,
     }
-  }
-}
-
-export const getStaticPaths: GetStaticPaths = async () => {
-  return {
-    paths: [],
-    fallback: true,
   }
 }
 
