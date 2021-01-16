2473943ad29cda9561659e3f0f8dc4c4d75c645e
Phumrapee Limpianchop <git@rayriffy.com>
2021-01-16 22:27:56 +0900

    fix: more page level

--- apps/web-next/src/core/components/listing.tsx
+++ apps/web-next/src/core/components/listing.tsx
@@ -18,11 +18,12 @@ const Component: React.FC<IProps> = React.memo(props => {
   const mediaMd = useMedia('(min-width: 768px)')
   const mediaLg = useMedia('(min-width: 1024px)')
   const mediaXl = useMedia('(min-width: 1280px)')
+  const media2Xl = useMedia('(min-width: 1536px)')
 
-  const column = useMemo(() => (mediaXl ? 5 : mediaLg ? 4 : mediaMd ? 3 : 2), [
-    mediaLg,
-    mediaMd,
-  ])
+  const column = useMemo(
+    () => (media2Xl ? 6 : mediaXl ? 5 : mediaLg ? 4 : mediaMd ? 3 : 2),
+    [mediaLg, mediaMd, mediaXl, media2Xl]
+  )
 
   return (
     <div className="flex flex-column flex-wrap">
@@ -36,11 +37,13 @@ const Component: React.FC<IProps> = React.memo(props => {
               ? 'w-1/3'
               : column === 4
               ? 'w-1/4'
-              : 'w-1/5'
+              : column === 5
+              ? 'w-1/5'
+              : 'w-1/6'
           }
         >
           {chunk.map(hentai => (
-            <div key={`poster-${hentai.id}`} className="p-3">
+            <div key={`poster-${hentai.id}`} className="p-3 flex items-center">
               <Poster hentai={hentai} />
             </div>
           ))}
