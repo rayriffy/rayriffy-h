f49b25855422866d9352594d13ae5211f55cef46
Phumrapee Limpianchop <git@rayriffy.com>
2021-03-13 17:56:21 +0000

    fix: navbar

--- apps/web-next/src/app/components/mobileOverlay.tsx
+++ apps/web-next/src/app/components/mobileOverlay.tsx
@@ -25,12 +25,9 @@ export const MobileOverlay: React.FC<IProps> = React.memo(props => {
           leave="transition-opacity ease-linear duration-300"
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
+          className="fixed inset-0"
         >
-          {ref => (
-            <div ref={ref} className="fixed inset-0">
-              <div className="absolute inset-0 bg-gray-600 opacity-75"></div>
-            </div>
-          )}
+          <div className="absolute inset-0 bg-gray-600 opacity-75"></div>
         </Transition.Child>
         <Transition.Child
           enter="transition ease-in-out duration-300 transform"
@@ -39,39 +36,29 @@ export const MobileOverlay: React.FC<IProps> = React.memo(props => {
           leave="transition ease-in-out duration-300 transform"
           leaveFrom="translate-x-0"
           leaveTo="-translate-x-full"
+          className="relative flex-1 flex flex-col max-w-xs w-full pt-5 pb-4 bg-white"
         >
-          {ref => (
-            <React.Fragment>
-              <div
-                ref={ref}
-                className="relative flex-1 flex flex-col max-w-xs w-full pt-5 pb-4 bg-white"
-              >
-                <div className="absolute top-0 right-0 -mr-14 p-1">
-                  <button
-                    onClick={onToggleSidebar}
-                    className="flex items-center justify-center h-12 w-12 rounded-full focus:outline-none focus:bg-gray-600"
-                    aria-label="Close sidebar"
-                  >
-                    <X className="h-6 w-6 text-white" />
-                  </button>
-                </div>
-                <div className="px-4">
-                  <p className="font-bold text-3xl text-gray-800">Riffy H</p>
-                  <p className="text-xs text-gray-500">
-                    {process.env.buildNumber}
-                  </p>
-                </div>
-                <Offline />
-                <div className="mt-5 flex-1 h-0 overflow-y-auto">
-                  <MobileNavbar />
-                </div>
-              </div>
-              <div className="flex-shrink-0 w-14">
-                {/* Dummy element to force sidebar to shrink to fit close icon */}
-              </div>
-            </React.Fragment>
-          )}
+          <div className="absolute top-0 right-0 -mr-14 p-1">
+            <button
+              onClick={onToggleSidebar}
+              className="flex items-center justify-center h-12 w-12 rounded-full focus:outline-none focus:bg-gray-600"
+              aria-label="Close sidebar"
+            >
+              <X className="h-6 w-6 text-white" />
+            </button>
+          </div>
+          <div className="px-4">
+            <p className="font-bold text-3xl text-gray-800">Riffy H</p>
+            <p className="text-xs text-gray-500">{process.env.buildNumber}</p>
+          </div>
+          <Offline />
+          <div className="mt-5 flex-1 h-0 overflow-y-auto">
+            <MobileNavbar />
+          </div>
         </Transition.Child>
+        <div className="flex-shrink-0 w-14">
+          {/* Dummy element to force sidebar to shrink to fit close icon */}
+        </div>
       </Transition>
     </div>
   )
