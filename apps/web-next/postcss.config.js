953a243f9343d3229071ebf00bb81f835bbd87f5
Phumrapee Limpianchop <git@rayriffy.com>
2021-04-09 18:50:19 +0000

    chore: staged changes

--- apps/web-next/postcss.config.js
+++ apps/web-next/postcss.config.js
@@ -1,27 +1,19 @@
 module.exports = {
-  plugins: [
-    'tailwindcss',
-    'postcss-nesting',
-    'postcss-flexbugs-fixes',
-    [
-      'cssnano',
-      {
-        preset: 'advanced',
-      },
-    ],
-    [
-      '@fullhuman/postcss-purgecss',
-      {
-        content: [
-          './src/**/*.{js,jsx,ts,tsx}',
-          './apps/web-next/src/**/*.{js,jsx,ts,tsx}',
-          './libs/datasource/src/**/*.{js,jsx,ts,tsx}',
-          './libs/icons/src/**/*.{js,jsx,ts,tsx}',
-          // './components/**/*.{js,jsx,ts,tsx}',
-        ],
-        safelist: ['html', 'body'],
-        defaultExtractor: content => content.match(/[\w-/:.%]+(?<!:)/g) || [],
-      },
-    ],
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    'postcss-nesting': {},
+    'postcss-flexbugs-fixes': {},
+    '@fullhuman/postcss-purgecss': {
+      content: [
+        './src/**/*.{js,jsx,ts,tsx}',
+        './apps/web-next/src/**/*.{js,jsx,ts,tsx}',
+        './libs/datasource/src/**/*.{js,jsx,ts,tsx}',
+        './libs/icons/src/**/*.{js,jsx,ts,tsx}',
+        // './components/**/*.{js,jsx,ts,tsx}',
+      ],
+      safelist: ['html', 'body'],
+      defaultExtractor: content => content.match(/[\w-/:.%]+(?<!:)/g) || [],
+    }
+  },
 }
