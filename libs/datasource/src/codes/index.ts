4778ec671607d1be9955065a051695e684ad5162
Phumrapee Limpianchop <git@rayriffy.com>
2021-05-03 20:20:03 +0000

    chore: add more data

--- libs/datasource/src/codes/index.ts
+++ libs/datasource/src/codes/index.ts
@@ -6,6 +6,7 @@ import { data2021_01 } from './months/2021-01'
 import { data2021_02 } from './months/2021-02'
 import { data2021_03 } from './months/2021-03'
 import { data2021_04 } from './months/2021-04'
+import { data2021_05 } from './months/2021-05'
 
 import { DatabaseCode } from '../@types/DatabaseCode'
 
@@ -16,3 +17,4 @@ export const codes: DatabaseCode[] = original
   .concat(data2021_02)
   .concat(data2021_03)
   .concat(data2021_04)
+  .concat(data2021_05)
