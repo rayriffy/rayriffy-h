b12d7bdc8d7194bb6b932d1a5139a552375700f5
Phumrapee Limpianchop <git@rayriffy.com>
2021-10-21 05:45:43 +0900

    refactor: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
       emerald: colors.emerald,
       teal: colors.teal,
       cyan: colors.cyan,
-      lightblue: colors.lightBlue,
+      sky: colors.sky,
       blue: colors.blue,
       indigo: colors.indigo,
       violet: colors.violet,
