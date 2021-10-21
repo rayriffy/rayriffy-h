87f621dd75a2003fb39e210077399ff5ef83971c
Phumrapee Limpianchop <git@rayriffy.com>
2021-10-22 00:40:43 +0900

    Revert "chore: staged changes"
    This reverts commit 886e489235b9c7d3c4058018db6340979099aece.

--- apps/web-next/src/pages/api/exp/[[...paths]].ts
+++ apps/web-next/src/pages/api/exp/[[...paths]].ts
@@ -7,7 +7,7 @@ const api: NextApiHandler = (req, res) => {
   const { paths = '/' } = req.query
   const filePath = path.join(process.cwd(), typeof paths === 'string' ? paths : paths.join('/'))
 
-  return res.status(200).send(fs.readdirSync(filePath).map(o => fs.statSync(path.join(filePath, o)).isDirectory() ? `${o}/` : o))
+  return res.status(200).send(fs.readdirSync(filePath))
 }
 
 export default api
