b167d4ea742e3bd63a311f527a389c974d1d0e53
Phumrapee Limpianchop <git@rayriffy.com>
2022-01-13 03:11:02 +0900

    chore: staged changes

--- apps/web-next/next.config.js
+++ apps/web-next/next.config.js
@@ -13,15 +13,13 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-const withPreact = require('next-plugin-preact')
-
 dayjs.extend(utc)
 dayjs.extend(timezone)
 
 const generatedId = nanoid()
 
 module.exports = withPlugins(
-  [[withWorkers], [withOffline], [withPreact], [withBundleAnalyzer]],
+  [[withWorkers], [withOffline], [withBundleAnalyzer]],
   {
     target: 'serverless',
     env: {
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,30 +3,11 @@ const colors = require('tailwindcss/colors')
 module.exports = {
   mode: 'jit',
   theme: {
-    colors: {
-      white: '#ffffff',
-      black: '#000000',
-      bluegray: colors.blueGray,
-      coolgray: colors.coolGray,
-      gray: colors.gray,
-      truegray: colors.trueGray,
-      warmgray: colors.warmGray,
-      red: colors.red,
-      orange: colors.orange,
-      amber: colors.amber,
-      yellow: colors.yellow,
-      lime: colors.lime,
-      green: colors.green,
-      emerald: colors.emerald,
-      teal: colors.teal,
-      cyan: colors.cyan,
-      sky: colors.sky,
-      blue: colors.blue,
-      indigo: colors.indigo,
-      violet: colors.violet,
-      purple: colors.purple,
-      pink: colors.pink,
-      rose: colors.rose,
+    extend: {
+      colors: {
+        white: '#ffffff',
+        black: '#000000',
+      },
     },
     filter: {
       none: 'none',
@@ -54,14 +35,11 @@ module.exports = {
     require('@tailwindcss/aspect-ratio'),
     require('tailwindcss-filters'),
   ],
-  purge: {
-    enabled: true,
-    content: [
-      './apps/web-next/src/**/*.{js,jsx,ts,tsx}',
-      './libs/constants/src/**/*.{js,jsx,ts,tsx}',
-      './libs/datasource/src/**/*.{js,jsx,ts,tsx}',
-      './libs/tags/src/**/*.{js,jsx,ts,tsx}',
-      './libs/icons/src/**/*.{js,jsx,ts,tsx}',
-    ],
-  }
+  content: [
+    './apps/web-next/src/**/*.{js,jsx,ts,tsx}',
+    './libs/constants/src/**/*.{js,jsx,ts,tsx}',
+    './libs/datasource/src/**/*.{js,jsx,ts,tsx}',
+    './libs/tags/src/**/*.{js,jsx,ts,tsx}',
+    './libs/icons/src/**/*.{js,jsx,ts,tsx}',
+  ],
 }
