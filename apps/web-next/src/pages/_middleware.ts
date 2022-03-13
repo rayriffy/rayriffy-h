a87f3b8a4465aadd7049e4ac1333db9d35cf5472
Phumrapee Limpianchop <git@rayriffy.com>
2022-03-14 05:39:52 +0900

    chore: staged changes

--- apps/web-next/src/pages/_middleware.ts
+++ apps/web-next/src/pages/_middleware.ts
@@ -1,6 +1,7 @@
 import { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
+  console.log(req.geo.country)
   if (['th'].includes(req.geo.country.toLowerCase())) {
     return NextResponse.next()
   } else {
