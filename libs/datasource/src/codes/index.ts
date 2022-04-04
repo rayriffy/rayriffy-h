858fb7369b9c67545705148addd99f1c979c02ba
Phumrapee Limpianchop <git@rayriffy.com>
2022-04-05 03:47:21 +0900

    chore: staged changes

--- libs/datasource/src/codes/index.ts
+++ libs/datasource/src/codes/index.ts
@@ -17,6 +17,7 @@ import { data2021_12 } from './months/2021-12'
 import { data2022_01 } from './months/2022-01'
 import { data2022_02 } from './months/2022-02'
 import { data2022_03 } from './months/2022-03'
+import { data2022_04 } from './months/2022-04'
 
 import { DatabaseCode } from '../@types/DatabaseCode'
 
@@ -38,3 +39,4 @@ export const codes: DatabaseCode[] = original
   .concat(data2022_01)
   .concat(data2022_02)
   .concat(data2022_03)
+  .concat(data2022_04)
