4f355b1d6d13ff9e2a4f679253d9d10eb2314fef
Phumrapee Limpianchop <git@rayriffy.com>
2022-04-07 08:37:55 +0900

    refactor: partytown

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,20 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
+import { Partytown } from '@builder.io/partytown/react'
 class NextDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
+        <Head>
+          <Partytown forward={['dataLayer.push']} />
+
           <script
             async
             src="https://www.googletagmanager.com/gtag/js?id=G-12TFVLTM16"
+            type="text/partytown"
           ></script>
           <script
+            type="text/partytown"
             dangerouslySetInnerHTML={{
               __html: `
                 window.dataLayer = window.dataLayer || [];
@@ -23,6 +25,10 @@ class NextDocument extends Document {
               `,
             }}
           />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
         </body>
       </Html>
     )
