2549474ed8512e59c3ee63f9358a97984cb175b9
Phumrapee Limpianchop <git@rayriffy.com>
2022-04-07 08:40:06 +0900

    chore: staged changes

--- src/core/components/imageBlur.tsx
+++ src/core/components/imageBlur.tsx
@@ -5,12 +5,12 @@ import Image from 'next/image'
 import { useStoreon } from '../../context'
 
 export const ImageBlur = memo<ComponentProps<typeof Image>>(props => {
-  const { className, ...rest } = props
+  const { className = '', ...rest } = props
   const { settings } = useStoreon('settings')
 
   return (
     <div
-      className={`${className} overflow-hidden ${
+      className={`${className} flex overflow-hidden ${
         settings.safemode ? 'blur-lg transform scale-105' : ''
       }`}
     >
