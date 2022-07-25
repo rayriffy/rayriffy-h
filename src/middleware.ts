4cf0e8086665283b8b0e58827fd5b6b6b630698b
Phumrapee Limpianchop <git@rayriffy.com>
2022-07-26 02:11:26 +0900

    feat: bump next

---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+const middleware = (req: NextRequest) => {
+  if (['robots.txt'].some(o => req.url.includes(o))) {
+    return NextResponse.next()
+  } else if (
+    process.env.NODE_ENV === 'development' ||
+    (['th', 'sg'].includes((req.geo?.country ?? '').toLowerCase()) &&
+      !(req.headers.get('referer') ?? '').startsWith(
+        'https://xn--vdkuc.xn--dck3c9b5d7d.xn--q9jyb4c'
+      ))
+  ) {
+    return NextResponse.next()
+  } else {
+    return new Response(undefined, {
+      status: 500,
+    })
+  }
+}
+
+export const config = {
+  matcher: ['/:path*'],
+}
+
+export default middleware
