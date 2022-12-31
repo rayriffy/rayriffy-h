6254dff348dc9555cc621996b7f8ac94b51d17f8
Phumrapee Limpianchop <git@rayriffy.com>
2023-01-01 02:20:58 +0900

    fix: pwa config

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,129 @@
 import { sveltekit } from '@sveltejs/kit/vite'
-import { VitePWA } from 'vite-plugin-pwa'
 import { SvelteKitPWA } from '@vite-pwa/sveltekit'
-
-import { runtimeCaching } from './runtimeCaching'
 
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [
     sveltekit(),
-    VitePWA({
-      // strategies: {},
+    SvelteKitPWA({
       registerType: 'autoUpdate',
       injectRegister: 'auto',
       workbox: {
-        runtimeCaching,
+        runtimeCaching: [
+          // optimized image
+          {
+            urlPattern: /\/api\/_image\?url=.+$/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'riffyh-svelte-optimized-image',
+              expiration: {
+                maxAgeSeconds: 60 * 60 * 24 * 30 * 6, // 6 months
+              },
+            },
+          },
+          // original nh image
+          {
+            urlPattern: /^https:\/\/[it]\.nhentai.net\/galleries\/\d+/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'riffyh-svelte-unoptimized-image',
+              expiration: {
+                maxAgeSeconds: 60 * 60 * 24 * 30, // 1 month
+              },
+            },
+          },
+          {
+            urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts',
+              expiration: {
+                maxEntries: 4,
+                maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
+              },
+            },
+          },
+          {
+            urlPattern: /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'static-font-assets',
+              expiration: {
+                maxEntries: 4,
+                maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
+              },
+            },
+          },
+          {
+            urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'static-image-assets',
+              expiration: {
+                maxEntries: 64,
+                maxAgeSeconds: 24 * 60 * 60, // 24 hours
+              },
+            },
+          },
+          {
+            urlPattern: /\.(?:js)$/i,
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'static-js-assets',
+              expiration: {
+                maxEntries: 32,
+                maxAgeSeconds: 24 * 60 * 60, // 24 hours
+              },
+            },
+          },
+          {
+            urlPattern: /\.(?:css|less)$/i,
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'static-style-assets',
+              expiration: {
+                maxEntries: 32,
+                maxAgeSeconds: 24 * 60 * 60, // 24 hours
+              },
+            },
+          },
+          {
+            urlPattern: /\.(?:json|xml|csv)$/i,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'static-data-assets',
+              expiration: {
+                maxEntries: 32,
+                maxAgeSeconds: 24 * 60 * 60, // 24 hours
+              },
+            },
+          },
+          {
+            urlPattern: /\/api\/.*$/i,
+            handler: 'NetworkFirst',
+            method: 'GET',
+            options: {
+              cacheName: 'apis',
+              expiration: {
+                maxEntries: 16,
+                maxAgeSeconds: 24 * 60 * 60, // 24 hours
+              },
+              networkTimeoutSeconds: 10, // fall back to cache if api does not response within 10 seconds
+            },
+          },
+          {
+            urlPattern: /.*/i,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'others',
+              expiration: {
+                maxEntries: 32,
+                maxAgeSeconds: 24 * 60 * 60, // 24 hours
+              },
+              networkTimeoutSeconds: 10,
+            },
+          },
+        ],
       },
     }),
   ],
