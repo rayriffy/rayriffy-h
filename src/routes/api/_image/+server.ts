d86b34ba909c1d7d5c7a5328b9a0b106636e0e08
Phumrapee Limpianchop <git@rayriffy.com>
2023-01-01 04:36:46 +0900

    chore: staged changes

--- src/routes/api/_image/+server.ts
+++ src/routes/api/_image/+server.ts
@@ -4,5 +4,8 @@ import type { RequestHandler } from './$types'
 
 export const GET: RequestHandler = requestHandler({
   remoteDomains: ['i.nhentai.net', 't.nhentai.net'],
-  allowedDomains: ['next.h.rayriffy.com'],
+  allowedDomains: [
+    'next.h.rayriffy.com',
+    'xn--vdkuc.xn--dck3c9b5d7d.xn--q9jyb4c',
+  ],
 })
