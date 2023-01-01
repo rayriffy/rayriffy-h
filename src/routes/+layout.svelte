9b60237760f44d1b1dff969b730cb875e04f16a4
Phumrapee Limpianchop <git@rayriffy.com>
2023-01-01 20:01:14 +0900

    fix: navigator ssr

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,26 @@
-<script>
+<script lang="ts">
   import '../styles/tailwind.css'
 
   import Navbar from '$core/components/Navbar.svelte'
-  import ReloadPrompt from '$core/components/ReloadPrompt.svelte'
 
   import { onMount } from 'svelte'
   import { provideStoreon } from '@storeon/svelte'
   import { store } from '$storeon'
   import { pwaInfo } from 'virtual:pwa-info'
 
+  import type { ComponentType } from 'svelte'
+
   provideStoreon(store)
 
+  let ReloadPrompt: ComponentType
   onMount(async () => {
     caches.delete('next-image-assets')
     caches.delete('next-galleries')
     caches.delete('next-listing')
+
+    pwaInfo &&
+      (ReloadPrompt = (await import('$core/components/ReloadPrompt.svelte'))
+        .default)
   })
 
   $: webManifest = pwaInfo ? pwaInfo.webManifest.linkTag : ''
@@ -25,8 +31,8 @@
 </svelte:head>
 
 <div class="app max-w-lg mx-auto">
-  {#if pwaInfo}
-    <ReloadPrompt />
+  {#if ReloadPrompt}
+    <svelte:component this={ReloadPrompt} />
   {/if}
   <main class="pb-16">
     <slot />
