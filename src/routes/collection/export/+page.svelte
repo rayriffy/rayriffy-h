7ad93b2d9177db6291c815386f5765cc87d8a1a3
Phumrapee Limpianchop <git@rayriffy.com>
2023-01-01 23:31:13 +0900

    feat: functional export

--- src/routes/collection/export/+page.svelte
+++ src/routes/collection/export/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
-  import type { APIResponse } from '$core/@types/APIResponse'
+  import ErrorIcon from '@svicons/boxicons-regular/error-circle.svelte'
+
   import { useStore } from '$storeon'
 
+  import type { APIResponse } from '$core/@types/APIResponse'
   import type { CollectionStore } from '$storeon/@types/CollectionStore'
 
   const { collection } = useStore('collection')
@@ -10,7 +12,7 @@
   let error: string | null = null
   let exportCode: string
 
-  const onExport = async (collection: CollectionStore) => {
+  const onExport = async (collection: CollectionStore['collection']) => {
     status = 'process'
     error = null
 
@@ -43,15 +45,44 @@
       <p class="text-gray-500">
         Transfer collection to other deivces by using temporary key.
       </p>
-      <div class="bg-info text-info-content p-4 rounded-xl">
-        <p>
-          You're about to exporting <b>{$collection.data.length} items</b> from
-          this device...press <b>Export</b> button to continue
-        </p>
-      </div>
-      <div class="card-actions justify-end pt-2">
-        <button class="btn btn-primary">Export</button>
-      </div>
+      {#if error !== null}
+        <div class="alert alert-error shadow-lg text-sm my-2">
+          <div>
+            <ErrorIcon class="flex-shrink-0 h-6 w-6" />
+            <span>{error}</span>
+          </div>
+        </div>
+      {/if}
+      {#if status === 'wait'}
+        <div class="alert alert-info shadow-lg text-sm my-2">
+          <div>
+            <span
+              >You're about to exporting <b>{$collection.data.length} items</b>
+              from this device...press <b>Export</b> button to continue</span
+            >
+          </div>
+        </div>
+        <div class="card-actions justify-end pt-2">
+          <button class="btn btn-primary" on:click={() => onExport($collection)}
+            >Export</button
+          >
+        </div>
+      {:else if status === 'process'}
+        <div class="pt-8 pb-2 flex flex-col items-center">
+          <progress class="progress w-56" />
+          <p class="text-base-content text-sm pt-2">Exporting...</p>
+        </div>
+      {:else if status === 'done'}
+        <div class="flex flex-col items-center">
+          <p class="text-base-content text-sm pt-2">Collection Exported! 👍🏼</p>
+          <div class="my-4 bg-base-200 px-6 py-4 font-mono  text-xl rounded-xl">
+            {exportCode}
+          </div>
+        </div>
+        <div class="card-actions justify-end pt-2">
+          <a href="/collection" class="btn btn-primary">Done</a>
+        </div>
+      {/if}
     </div>
   </div>
 </section>
