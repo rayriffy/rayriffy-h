47d6eed03b34ea6d96d9414c6e9e7b631026857b
Phumrapee Limpianchop <git@rayriffy.com>
2023-03-06 11:37:16 +0900

    chore: mount dataset

--- src/core/constants/codes/2023/index.ts
+++ src/core/constants/codes/2023/index.ts
@@ -1,8 +1,10 @@
 import { data2023_01 } from './2023-01'
 import { data2023_02 } from './2023-02'
+import { data2023_03 } from './2023-03'
 
 import type { DatabaseCode } from '$core/@types/DatabaseCode'
 
 export const year2023 = ([] as DatabaseCode[])
   .concat(data2023_01)
   .concat(data2023_02)
+  .concat(data2023_03)
