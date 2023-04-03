a34b3c80213eb9bca3de51b5a224df56413e5e13
Phumrapee Limpianchop <git@rayriffy.com>
2023-04-04 02:15:59 +0900

    feat: add more data

---
+++ src/core/constants/codes/2023/2023-04.ts
@@ -0,0 +1,14 @@
+import type { DatabaseCode } from '$core/@types/DatabaseCode'
+
+export const data2023_04: DatabaseCode[] = [
+  449069,
+  449057,
+  449022,
+  449018,
+  448948,
+  448925,
+  448900,
+  448867,
+  448829,
+  448810,
+]
--- src/core/constants/codes/2023/index.ts
+++ src/core/constants/codes/2023/index.ts
@@ -1,6 +1,7 @@
 import { data2023_01 } from './2023-01'
 import { data2023_02 } from './2023-02'
 import { data2023_03 } from './2023-03'
+import { data2023_04 } from './2023-04'
 
 import type { DatabaseCode } from '$core/@types/DatabaseCode'
 
@@ -8,3 +9,4 @@ export const year2023 = ([] as DatabaseCode[])
   .concat(data2023_01)
   .concat(data2023_02)
   .concat(data2023_03)
+  .concat(data2023_04)
