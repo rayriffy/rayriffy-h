2c56997af341444d8877f982dfa108118372fa03
Phumrapee Limpianchop <git@rayriffy.com>
2023-06-02 08:14:13 +0900

    chore: staged changes

--- src/core/constants/codes/2023/2023-06.ts
+++ src/core/constants/codes/2023/2023-06.ts
@@ -2,5 +2,5 @@ import type { DatabaseCode } from '$core/@types/DatabaseCode'
 
 export const data2023_06: DatabaseCode[] = [
   279262, 299884, 457516, 457440, 457427, 457418, 367750, 457398, 457362,
-  457335, 457279, 457243, 457231,
+  457335, 457279, 457243, 457231, 227753
 ]
