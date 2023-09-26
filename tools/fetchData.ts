340dc7ab459280ddc812624a451d5a4c5e948d52
Phumrapee Limpianchop <git@rayriffy.com>
2023-09-27 04:04:34 +0900

    refactor: top-level await is available

--- tools/fetchData.ts
+++ tools/fetchData.ts
@@ -17,87 +17,87 @@ const fetchQueue = new PQueue({
   concurrency: 8,
 })
 
-;(async () => {
-  // generating chunks
-  console.log('generating chunks')
-  const prebuiltChunkDirectory = path.join(cacheDirectory, 'prebuiltChunks')
+// generating chunks
+console.log('generating chunks')
+const prebuiltChunkDirectory = path.join(cacheDirectory, 'prebuiltChunks')
 
-  if (!fs.existsSync(prebuiltChunkDirectory)) {
-    await fs.promises.mkdir(prebuiltChunkDirectory, { recursive: true })
-  }
+if (!fs.existsSync(prebuiltChunkDirectory)) {
+  await fs.promises.mkdir(prebuiltChunkDirectory, { recursive: true })
+}
 
-  const chunks = chunk(codes.reverse(), itemsPerPage)
+const chunks = chunk(codes.reverse(), itemsPerPage)
 
-  await Promise.all(
-    chunks.map(async (chunk, i) => {
-      const chunkFile = path.join(prebuiltChunkDirectory, `chunk-${i + 1}.json`)
-      await fs.promises.writeFile(
-        chunkFile,
-        JSON.stringify(chunk.map(o => (typeof o === 'number' ? o : o.code)))
-      )
-    })
-  )
+await Promise.all(
+  chunks.map(async (chunk, i) => {
+    const chunkFile = path.join(prebuiltChunkDirectory, `chunk-${i + 1}.json`)
+    await fs.promises.writeFile(
+      chunkFile,
+      JSON.stringify(chunk.map(o => (typeof o === 'number' ? o : o.code)))
+    )
+  })
+)
 
-  console.log('fetching all galleries')
-  const hentaiDirectory = path.join(cacheDirectory, 'hentai')
+console.log('fetching all galleries')
+const hentaiDirectory = path.join(cacheDirectory, 'hentai')
 
-  if (!fs.existsSync(hentaiDirectory)) {
-    await fs.promises.mkdir(hentaiDirectory, { recursive: true })
-  }
+if (!fs.existsSync(hentaiDirectory)) {
+  await fs.promises.mkdir(hentaiDirectory, { recursive: true })
+}
 
-  let hasError
-  let prisma: PrismaClient | undefined
+let hasError
+let prisma: PrismaClient | undefined
 
-  if (process.env.DATABASE_URL !== undefined) {
-    prisma = new PrismaClient()
-  }
+if (process.env.DATABASE_URL !== undefined) {
+  prisma = new PrismaClient()
+}
 
-  await Promise.all(
-    codes.map(code =>
-      fetchQueue.add(() => getRemoteHentai(code, prisma).catch(() => {
+await Promise.all(
+  codes.map(code =>
+    fetchQueue.add(() =>
+      getRemoteHentai(code, prisma).catch(() => {
         hasError = true
-      }))
+      })
     )
   )
+)
 
-  await fetchQueue.onIdle()
+await fetchQueue.onIdle()
 
-  if (hasError) {
-    console.error("there's some error during fetching! crashing...")
-    process.exit(1)
-  } else {
-    console.log('fetched all galleries')
-  }
+if (hasError) {
+  console.error("there's some error during fetching! crashing...")
+  process.exit(1)
+} else {
+  console.log('fetched all galleries')
+}
 
-  /**
-   * Create search keys for searching
-   */
-  const orderedHentai = codes
-    .map(code => {
-      try {
-        const targetCode = typeof code === 'number' ? code : code.code
-        const targetHentai = destr<Hentai>(
-          fs
-            .readFileSync(path.join(hentaiDirectory, `${targetCode}.json`))
-            .toString()
-        )
+/**
+ * Create search keys for searching
+ */
+const orderedHentai = codes
+  .map(code => {
+    try {
+      const targetCode = typeof code === 'number' ? code : code.code
+      const targetHentai = destr<Hentai>(
+        fs
+          .readFileSync(path.join(hentaiDirectory, `${targetCode}.json`))
+          .toString()
+      )
 
-        const transformedHentai: Hentai = {
-          ...targetHentai,
-          images: {
-            ...targetHentai.images,
-            pages: [],
-          },
-        }
-
-        return transformedHentai
-      } catch (e) {
-        return null
+      const transformedHentai: Hentai = {
+        ...targetHentai,
+        images: {
+          ...targetHentai.images,
+          pages: [],
+        },
       }
-    })
-    .filter(o => o !== null)
-  await fs.promises.writeFile(
-    path.join(cacheDirectory, 'searchKey.json'),
-    JSON.stringify(orderedHentai)
-  )
-})()
+
+      return transformedHentai
+    } catch (e) {
+      return null
+    }
+  })
+  .filter(o => o !== null)
+await fs.promises.writeFile(
+  path.join(cacheDirectory, 'searchKey.json'),
+  JSON.stringify(orderedHentai)
+)
--- tools/syncData.ts
+++ tools/syncData.ts
@@ -8,46 +8,43 @@ import type { Hentai } from '../src/core/@types/Hentai'
 
 const hentaiDirectory = path.join(process.cwd(), 'data/hentai')
 
-;(async () => {
-  if (process.env.DATABASE_URL === undefined) {
-    return
-  }
+if (process.env.DATABASE_URL === undefined)
+  process.exit(0)
 
-  const prisma = new PrismaClient()
-  try {
-    // list ids in local cache
-    const ids = await prisma.hentai.findMany({
-      select: {
-        id: true,
-      },
-    })
-    // push new data to local cache
-    const itemsToPush = fs
-      .readdirSync(hentaiDirectory)
-      .filter(o => !o.startsWith('.') && o.endsWith('.json'))
-      .filter(
-        o =>
-          !ids
-            .map(p => p.id)
-            .includes(
-              destr<Hentai>(
-                fs.readFileSync(path.join(hentaiDirectory, o), 'utf8')
-              ).id
-            )
+const prisma = new PrismaClient()
+try {
+  // list ids in local cache
+  const ids = await prisma.hentai.findMany({
+    select: {
+      id: true,
+    },
+  })
+  // push new data to local cache
+  const itemsToPush = fs
+    .readdirSync(hentaiDirectory)
+    .filter(o => !o.startsWith('.') && o.endsWith('.json'))
+    .filter(
+      o =>
+        !ids
+          .map(p => p.id)
+          .includes(
+            destr<Hentai>(
+              fs.readFileSync(path.join(hentaiDirectory, o), 'utf8')
+            ).id
+          )
+    )
+    .map(file => {
+      return destr<Hentai>(
+        fs.readFileSync(path.join(hentaiDirectory, file), 'utf8')
       )
-      .map(file => {
-        return destr<Hentai>(
-          fs.readFileSync(path.join(hentaiDirectory, file), 'utf8')
-        )
-      })
-    await prisma.hentai.createMany({
-      data: itemsToPush.map(item => ({
-        id: item.id,
-        data: JSON.stringify(item),
-      })),
     })
-  } catch (e) {
-    console.log(e)
-    await prisma.$disconnect()
-  }
-})()
+  await prisma.hentai.createMany({
+    data: itemsToPush.map(item => ({
+      id: item.id,
+      data: JSON.stringify(item),
+    })),
+  })
+} catch (e) {
+  console.log(e)
+  await prisma.$disconnect()
+}
