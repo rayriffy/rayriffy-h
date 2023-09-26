ed6b963c23d706cf408ee8cba6b482db28f0afa5
Phumrapee Limpianchop <git@rayriffy.com>
2023-09-27 04:12:40 +0900

    refactor: patch script to top-level await

--- tools/patchSW.ts
+++ tools/patchSW.ts
@@ -1,28 +1,24 @@
 import fs from 'fs'
 import path from 'path'
-;(async () => {
-  await Promise.allSettled(
-    [
-      path.join(process.cwd(), '.svelte-kit/output/client/sw.js'),
-      path.join(process.cwd(), 'build/client/sw.js'),
-    ].map(async filePath => {
-      const originalContent = await fs.promises.readFile(filePath, 'utf8')
 
-      const targetString =
-        'e.registerRoute(new e.NavigationRoute(e.createHandlerBoundToURL("/"))),'
+await Promise.allSettled(
+  [
+    path.join(process.cwd(), '.svelte-kit/output/client/sw.js'),
+    path.join(process.cwd(), 'build/client/sw.js'),
+  ].map(async filePath => {
+    const originalContent = await fs.promises.readFile(filePath, 'utf8')
 
-      if (originalContent.includes(targetString))
-        await fs.promises.writeFile(
-          filePath,
-          originalContent.replace(
-            `e.registerRoute(new e.NavigationRoute(e.createHandlerBoundToURL("/"))),`,
-            ''
-          )
+    const targetString =
+      'e.registerRoute(new e.NavigationRoute(e.createHandlerBoundToURL("/"))),'
+
+    if (originalContent.includes(targetString))
+      await fs.promises.writeFile(
+        filePath,
+        originalContent.replace(
+          `e.registerRoute(new e.NavigationRoute(e.createHandlerBoundToURL("/"))),`,
+          ''
         )
-      else throw new Error('no replacement found')
-    })
-  )
-})().catch(e => {
-  console.error(e)
-  process.exit(1)
-})
+      )
+    else throw new Error('no replacement found')
+  })
+)
