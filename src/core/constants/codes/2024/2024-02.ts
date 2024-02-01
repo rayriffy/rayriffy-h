7cae3ba649ab0d7179ee210d620ce676cafc6589
Phumrapee Limpianchop <git@rayriffy.com>
2024-02-02 07:51:00 +0900

    chore: staged changes

---
+++ src/core/constants/codes/2024/2024-02.ts
@@ -0,0 +1,6 @@
+import type { DatabaseCode } from '$core/@types/DatabaseCode'
+
+export const data2024_02: DatabaseCode[] = [
+  494443, 494375, 494259, 494246, 494223, 494221, 494202, 324533, 494186,
+  494185, 494172, 494092, 494083, 493988, 466073, 486130, 493958, 493887,
+]
--- src/core/constants/codes/2024/index.ts
+++ src/core/constants/codes/2024/index.ts
@@ -1,6 +1,8 @@
 import { data2024_01 } from './2024-01'
+import { data2024_02 } from './2024-02'
 
 import type { DatabaseCode } from '$core/@types/DatabaseCode'
 
 export const year2024 = ([] as DatabaseCode[])
   .concat(data2024_01)
+  .concat(data2024_02)
