6c42198dd68a4d123fae0ffb07f2b3fac773191a
Phumrapee Limpianchop <git@rayriffy.com>
2025-06-16 01:59:21 +0700

    chore: staged changes

--- commons/src/functions/getHentaiFromNH.ts
+++ commons/src/functions/getHentaiFromNH.ts
@@ -2,7 +2,11 @@ import { sanitizeContent } from './sanitizeContent'
 import type { Hentai } from '../@types/Hentai'
 
 export const getHentaiFromNH = async (id: string | number): Promise<Hentai> => {
-  const data = await fetch(`https://nhentai.net/api/gallery/${id}`).then(o => {
+  const urlPattern = process.env.NH_PATTERN || 'https://nhentai.net/api/gallery/{0}'
+
+  const data = await fetch(
+    urlPattern.replace('{0}', id.toString())
+  ).then(o => {
     if (o.ok) return o.json() as Promise<Hentai>
     else throw o
   })
